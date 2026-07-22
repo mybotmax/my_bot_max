d7896a574b47f2dc5b7b204241b69f5a5ea0ea66
mybotmax <zorjamba@mail.ru>
2026-07-22 22:54:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,7 @@ import requests
 import streamlit as st
 import json
 
-BOT_TOKEN = "f9LHodD0cOJDvMkvHcYvQ_WXz46iuVcrUsoYaH7QLRQ799cTzdNwqAxxCj7qgX8D4a42anK0_SA86LkhmoAC"
-
-# ВАЖНО: Это правильный адрес из документации!
+BOT_TOKEN = "f9LHodD0cOLNe5TAyASUtuyV1Dl4KEGrYkDpQyaZmTfbiZP3C7PmL46-VL8-Z7Gr808kj6ajFQWpTEoTvLzD"
 SEND_URL = "https://platform-api2.max.ru/messages"
 
 st.title("🤖 Мой Макс Бот")
@@ -17,11 +15,12 @@ text = params.get("text")
 if chat_id:
     st.success(f"✅ Получено: {text}")
     
-    headers = {"Authorization": BOT_TOKEN, "Content-Type": "application/json"}
+    # ⚠️ ВОТ ЗДЕСЬ ВАЖНОЕ ИЗМЕНЕНИЕ: ДОБАВИЛИ Bearer
+    headers = {"Authorization": f"Bearer {BOT_TOKEN}", "Content-Type": "application/json"}
+    
     data = {"chatId": chat_id, "text": f"Привет! Я получил: '{text}'"}
     
     try:
-        # Отправляем запрос на правильный адрес
         json_data = json.dumps(data, ensure_ascii=False).encode('utf-8')
         response = requests.post(SEND_URL, headers=headers, data=json_data, timeout=5, verify=False)
         
